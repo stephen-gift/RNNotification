93ff72896a4ac06a478f2909e7abaef275b017b1
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-01-29 00:31:06 +0100

    local notifications

--- App.js
+++ App.js
@@ -1,16 +1,55 @@
 import { StatusBar } from "expo-status-bar";
 import { Button, StyleSheet, Text, View } from "react-native";
 import * as Notifications from "expo-notifications";
+import { useEffect } from "react";
 
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+  }),
+});
 export default function App() {
-  const schduleNotificationHandler = () => {
+  useEffect(() => {
+    const getNotificationPermission = async () => {
+      const { status } = await Notifications.requestPermissionsAsync();
+      if (status !== "granted") {
+        alert(
+          "Notification permissions are required to schedule notifications."
+        );
+      }
+    };
+
+    getNotificationPermission();
+
+    const subscription1 = Notifications.addNotificationReceivedListener(
+      (notification) => {
+        console.log("NOTIFICATION RECEIVED");
+        console.log(notification);
+        const userName = notification.request.content.data.userName;
+        console.log(userName);
+      }
+    );
+    const subscription2 = Notifications.addNotificationResponseReceivedListener(
+      (response) => {
+        console.log("NOTIFICATION RESPONSE RECEIVED");
+        console.log(response);
+      }
+    );
+    return () => {
+      subscription1.remove();
+      subscription2.remove();
+    };
+  }, []);
+  const scheduleNotificationHandler = () => {
     Notifications.scheduleNotificationAsync({
       content: {
         title: "My first local Notification",
         body: "This is the body of the scheduled notification",
+        vibrate: true,
         data: {
           userName: "Stephen",
-          date: new Date().toISOString(),
         },
       },
       trigger: {
@@ -22,7 +61,7 @@ export default function App() {
     <View style={styles.container}>
       <Button
         title="Schedule Notification"
-        onPress={schduleNotificationHandler}
+        onPress={scheduleNotificationHandler}
       />
       <StatusBar style="auto" />
     </View>
