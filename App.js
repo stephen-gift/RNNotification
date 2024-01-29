caad62d7f6ffbfc56de9fcd5b3162098ecd46c34
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-01-29 12:39:04 +0100

    restart

--- App.js
+++ App.js
@@ -1,68 +1,10 @@
-import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, View } from "react-native";
-import * as Notifications from "expo-notifications";
-import { useEffect } from "react";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-Notifications.setNotificationHandler({
-  handleNotification: async () => ({
-    shouldShowAlert: true,
-    shouldPlaySound: true,
-    shouldSetBadge: false,
-  }),
-});
 export default function App() {
-  useEffect(() => {
-    const getNotificationPermission = async () => {
-      const { status } = await Notifications.requestPermissionsAsync();
-      if (status !== "granted") {
-        alert(
-          "Notification permissions are required to schedule notifications."
-        );
-      }
-    };
-
-    getNotificationPermission();
-
-    const subscription1 = Notifications.addNotificationReceivedListener(
-      (notification) => {
-        console.log("NOTIFICATION RECEIVED");
-        console.log(notification);
-        const userName = notification.request.content.data.userName;
-        console.log(userName);
-      }
-    );
-    const subscription2 = Notifications.addNotificationResponseReceivedListener(
-      (response) => {
-        console.log("NOTIFICATION RESPONSE RECEIVED");
-        console.log(response);
-      }
-    );
-    return () => {
-      subscription1.remove();
-      subscription2.remove();
-    };
-  }, []);
-  const scheduleNotificationHandler = () => {
-    Notifications.scheduleNotificationAsync({
-      content: {
-        title: "My first local Notification",
-        body: "This is the body of the scheduled notification",
-        vibrate: true,
-        data: {
-          userName: "Stephen",
-        },
-      },
-      trigger: {
-        seconds: 5,
-      },
-    });
-  };
   return (
     <View style={styles.container}>
-      <Button
-        title="Schedule Notification"
-        onPress={scheduleNotificationHandler}
-      />
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -71,8 +13,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
